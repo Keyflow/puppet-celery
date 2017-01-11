1e671a1a9ab5bd6802f0dc3933a37d3951ad3570
Karl Johan Westrin <karl-johan.westrin@keyflow.com>
2017-01-11 15:47:57 +0100

    Finished: Refactoring for version 4 of celery.

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,55 +1,82 @@
 class celery::server($python_env="/usr",
                      $celery_env="/usr/local",
                      $proroot="",
-                     $venvowner="root",
-                     $requirements="/tmp/celery-requirements.txt",
-                     $requirements_template="celery/requirements.txt",
-                     $initd_template="celery/init.d.sh",
-                     $defaults_template="celery/celery_defaults.sh.erb",
-                     $broker_user="some_user",
-                     $broker_vhost="some_vhost",
-                     $broker_password="CHANGEME",
+                     $broker_prefix="amqp",
+                     $broker_user=undef,
+                     $broker_vhost=undef,
+                     $broker_password=undef,
                      $broker_host="localhost",
-                     $broker_port="5672",
+                     $broker_port=undef,
+                     $backend_prefix="amqp",
+                     $backend_user=undef,
+                     $backend_suffix=undef,
+                     $backend_password=undef,
+                     $backend_host="localhost",
+                     $backend_port=undef,
+                     $backend_url=undef,
                      $user="celery",
                      $group="celery",
+                     $celeryconfig_dir = '/var/celery',
                      $concurrency = '8',
                      $pypath_appendage = '',
                      $environment_vars = {},
                      $celery_app = '',
-                     $extra_opts) {
-  file { $requirements:
-    ensure => "present",
-    content => template($requirements_template),
+                     $extra_opts='') {
+  if ! defined('python') {
+    class { 'python':
+      version => 'system',
+      pip     => 'present',
+      dev     => 'present',
+    }
+  } else {
+    include 'python'
   }
+  file { '/tmp/celery-requirements.txt':
+    ensure => "present",
+    content => template("celery/requirements.txt"),
+    require => Class['python'],
+  } ->
+  python::requirements { '/tmp/celery-requirements.txt': }
 
   file { "/etc/default/celeryd":
     ensure => "present",
-    content => template($defaults_template),
+    content => template("celery/celery_defaults.sh.erb"),
   }
 
   file { "/etc/init.d/celeryd":
     ensure => "present",
-    content => template($initd_template),
+    content => template("celery/init.d.sh"),
     mode => "0755",
   }
  
-  file { "/var/celery":
+  user { $user:
+    ensure     => 'present',
+    gid        => $group,
+  } ->
+  file { "${celeryconfig_dir}":
     ensure   => "directory",
     owner    => $user,
     group    => $group,
+  } ->
+  file { "${celeryconfig_dir}/celeryconfig.py":
+    ensure  => "present",
+    content => template("celery/celeryconfig.py")
+    owner   => $user,
+    group   => $group,
   }
 
   file { "/var/log/celery":
-    ensure => "directory",
-    owner  => $user,
-    group  => $group,
+    ensure  => "directory",
+    owner   => $user,
+    group   => $group,
+    require => User[$user],
   }
 
   file { "/var/run/celery":
-    ensure => "directory",
-    owner  => $user,
-    group  => $group,
+    ensure  => "directory",
+    owner   => $user,
+    group   => $group,
+    require => User[$user],
   }
 
   service { "celeryd":
@@ -58,7 +85,6 @@ class celery::server($python_env="/usr",
     require => [File["/etc/init.d/celeryd"],
                 File["/etc/default/celeryd"],
                 File["/var/log/celery"],
-                File["/var/run/celery"],
-                Class["rabbitmq::service"], ],
+                File["/var/run/celery"], ],
   }
 }
--- templates/celeryconfig.py
+++ templates/celeryconfig.py
@@ -1,5 +1,6 @@
-BROKER_HOST = "<%= @broker_host %>"
-BROKER_PORT = <%= @broker_port %>
-BROKER_USER = "<%= @broker_user %>"
-BROKER_PASSWORD = "<%= @broker_password %>"
-BROKER_VHOST = "<%= @broker_vhost %>"
+broker_url = "<%= @broker_prefix %>://<% if @broker_user -%><%= @broker_user %><% if @broker_password -%>:<%= @broker_password %><% end -%><% end -%>@<%= @broker_host %><% if @broker_port %>:<%= @broker_port %><% end -%><% if @broker_vhost -%>/<%= @broker_vhost %><% end -%>"
+<% if @backend_url -%>
+result_backend = "<%= @backend_url %>"
+<% else -%>
+result_backend = "<%= @backend_prefix %>://<% if @backend_user -%><%= @backend_user %><% if @backend_password -%>:<%= @backend_password %><% end -%><% end -%>@<%= @backend_host %><% if @backend_port %>:<%= @backend_port %><% end -%><% if @backend_suffix -%>/<%= @backend_suffix %><% end -%>"
+<% end -%>
