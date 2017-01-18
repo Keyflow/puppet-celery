cf96818040f29f73aa2e53991b17a0976bf86de0
Karl Johan Westrin <karl-johan.westrin@keyflow.com>
2017-01-18 16:12:08 +0100

    Tweak: Possible to use more than one backend host.

--- templates/celeryconfig.py
+++ templates/celeryconfig.py
@@ -2,5 +2,5 @@ broker_url = "<%= @broker_prefix %>://<% if @broker_user -%><%= @broker_user %><
 <% if @backend_url -%>
 result_backend = "<%= @backend_url %>"
 <% else -%>
-result_backend = "<%= @backend_prefix %>://<% if @backend_user -%><%= @backend_user %><% if @backend_password -%>:<%= @backend_password %><% end -%><% end -%>@<%= @backend_host %><% if @backend_port %>:<%= @backend_port %><% end -%><% if @backend_suffix -%>/<%= @backend_suffix %><% end -%>"
+result_backend = "<%= @backend_prefix %>://<% if @backend_user -%><%= @backend_user %><% if @backend_password -%>:<%= @backend_password %><% end -%>@<% end -%><%= @backend_hosts.join(',') %><% if @backend_suffix -%>/<%= @backend_suffix %><% end -%>"
 <% end -%>
