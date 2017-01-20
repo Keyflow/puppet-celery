91c3754c50d3e1c1409c2b169e6075bec088aede
Karl Johan Westrin <karl-johan.westrin@keyflow.com>
2017-01-20 09:59:34 +0100

    Tweak: Celerybeats defaults file.

--- manifests/beat.pp
+++ manifests/beat.pp
@@ -1,16 +1,24 @@
 class celery::beat(
   $ensure="running",
+  $celerybeat_schedule_file="/var/run/celery/celerybeat-schedule",
 ) {
 
   service { "celerybeat":
     hasrestart => true,
     ensure     => $ensure,
-    subscribe  => [Class['celery::server'], File['/etc/init.d/celerybeat']],
+    subscribe  => [Class['celery::server'],
+                   File['/etc/init.d/celerybeat'],
+                   File['/etc/default/celerybeat']],
   }
 
   file { "/etc/init.d/celerybeat":
     ensure  => "present",
     content => template("celery/init.beat.sh"),
     mode    => "0755",
+  }
+
+  file { "/etc/default/celerybeat":
+    ensure  => "present",
+    content => template("celery/celerybeat_defaults.sh.erb"),
   }
 }
